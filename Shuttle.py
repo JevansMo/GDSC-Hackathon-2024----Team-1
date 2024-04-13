3efb5af49a9ca2b131739b4033f8d5f77be84662
JevansMo <jevansmo@xula.edu>
2024-04-13 14:46:37 -0500

    create eta method to give approximate arrival time to Xavier

--- Shuttle.py
+++ Shuttle.py
@@ -1,12 +1,23 @@
+from datetime import datetime, date, time, timedelta
+
 class shuttle:
 
     def __init__(self, dst: int, pop: int, type: str, cap: int):
         # set the instance variables to ints under the assumption that it's easier
         # for html buttons to return ints -Jalen
-        self.dst = "LSU" if dst == 0 else self.dst = "UNO"
+        dst = "LSU" if dst == 0 else "UNO"
         self.pop = pop
         # designates what type shuttle (van or bus) -Jalen
         self.type = type
         # how many people can fit the vehicle -Jalen
         self.cap = cap
+
+    # distance should be given in miles
+    def eta(self, distance: float) -> time:
+        # assuming speed limit is 40mph and converting into seconds -Jalen
+        result = datetime.now()
+        seconds = (distance/30)*(60**2)
+        result += timedelta(seconds=seconds)
+        return result.time()
+
 
