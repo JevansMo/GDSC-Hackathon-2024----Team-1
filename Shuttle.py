dac0d0d5638699980b6e58922530205f833cff30
JevansMo <jevansmo@xula.edu>
2024-04-13 15:19:31 -0500

    more descriptive name for eta method

--- Shuttle.py
+++ Shuttle.py
@@ -5,7 +5,7 @@ class shuttle:
     def __init__(self, dst: int, pop: int, type: str, cap: int):
         # set the instance variables to ints under the assumption that it's easier
         # for html buttons to return ints -Jalen
-        dst = "LSU" if dst == 0 else "UNO"
+        self.dst = "LSU" if dst == 0 else "UNO"
         self.pop = pop
         # designates what type shuttle (van or bus) -Jalen
         self.type = type
@@ -13,11 +13,13 @@ class shuttle:
         self.cap = cap
 
     # distance should be given in miles
-    def eta(self, distance: float) -> time:
+    def eta_to_xula(self, distance: float) -> time:
         # assuming speed limit is 40mph and converting into seconds -Jalen
         result = datetime.now()
         seconds = (distance/30)*(60**2)
         result += timedelta(seconds=seconds)
         return result.time()
+
+
 
 
