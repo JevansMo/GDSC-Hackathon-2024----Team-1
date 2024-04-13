bc948e7dc42dac6254f9c654c7bc0a8af9125914
JevansMo <jevansmo@xula.edu>
2024-04-13 12:20:08 -0500

    add more variables for shuttle class

--- Shuttle.py
+++ Shuttle.py
@@ -1,8 +1,12 @@
 class shuttle:
 
-    def __init__(self, dst: int, pop: int):
+    def __init__(self, dst: int, pop: int, type: str, cap: int):
         # set the instance variables to ints under the assumption that it's easier
         # for html buttons to return ints -Jalen
         self.dst = "LSU" if dst == 0 else self.dst = "UNO"
         self.pop = pop
+        # designates what type shuttle (van or bus) -Jalen
+        self.type = type
+        # how many people can fit the vehicle -Jalen
+        self.cap = cap
 
