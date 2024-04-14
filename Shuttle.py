d9a8e27cb1240abd8cc5247fadd1db7cb8ed3c4e
JevansMo <jevansmo@xula.edu>
2024-04-14 11:25:41 -0500

    merge with weather branch

--- Shuttle.py
+++ Shuttle.py
@@ -1,11 +1,10 @@
 from datetime import datetime, date, time, timedelta
 
-class shuttle:
-
-    def __init__(self, dst: int, pop: int, type: str, cap: int):
+class Shuttle:
+    def __init__(self, dst: str, pop: int, type: str, cap: int):
         # set the instance variables to ints under the assumption that it's easier
         # for html buttons to return ints -Jalen
-        self.dst = "LSU" if dst == 0 else "UNO"
+        self.dst = dst
         self.pop = pop
         # designates what type shuttle (van or bus) -Jalen
         self.type = type
@@ -13,9 +12,9 @@ class shuttle:
         self.cap = cap
 
     # distance should be given in miles
-    def eta_to_xula(self, distance: float) -> time:
+    def eta_to_xula(self, distance: float, delay: float) -> str:
         # assuming speed limit is 40mph and converting into seconds -Jalen
-        result = datetime.now()
-        seconds = (distance/30)*(60**2)
-        result += timedelta(seconds=seconds)
-        return result.time()
+        now = datetime.now()
+        seconds = (distance/20)*(60**2) + delay
+        result = (now + timedelta(seconds=seconds)).strftime("%I:%M %p")
+        return result
